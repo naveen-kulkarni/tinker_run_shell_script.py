b3e2077e2ef9cc55802feda05e621c0427e28705
naveen-kulkarni <33716521+naveen-kulkarni@users.noreply.github.com>
2023-04-16 22:52:14 +0530

    Add files via upload

---
+++ tinker.py
@@ -0,0 +1,17 @@
+import Tkinter
+import subprocess
+
+top = Tkinter.Tk()
+
+def helloCallBack():
+   print "Below is the output from the shell script in terminal"
+   subprocess.call('./yourscript.sh', shell=True)
+
+
+B = Tkinter.Button(top, text ="Hello", command = helloCallBack)
+
+B.pack()
+top.mainloop()
+
+
+
